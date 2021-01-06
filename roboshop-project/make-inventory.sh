262cd47c37e591988a2babb8cde2042e54594b80
KalluruS <>
2021-01-06 07:48:01 +0530

    Update ansible/

---
+++ roboshop-project/make-inventory.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+>inventory
+for component in frontend catalogue cart user shipping payment mysql mongo rabbitmq redis; do
+  IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq '.Reservations[].Instances[].PrivateIpAddress')
+  if [ -n "${IP}" ]; then
+    echo $IP component=${component} >>inventory
+  fi
+done
